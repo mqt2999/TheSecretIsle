288ec201196942890af7c2d0c86c81356d993365
Garrett Fite <36345799+Stranded-Helix@users.noreply.github.com>
2021-07-19 21:01:49 -0500

    changed file structure to match folder/index system

--- client/src/App.js
+++ client/src/App.js
@@ -3,7 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import './App.css';
 import Navbar from "./components/Navbar";
-import Story from './components/Story.js';
+import Story from './components/Story';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import SignUp from './pages/SignUp';
@@ -15,9 +15,9 @@ function App() {
       <div>
         <Navbar />
           <Route exact path="/Story" component={Story} />
-          <Route exact path="/Home" component={Home} />
           <Route exact path="/Login" component={Login} />
           <Route exact path="/SignUp" component={SignUp} />
+          <Route exact path="/" component={Home} />
       </div>
     </Router>
   );
--- client/src/components/Story/index.js
+++ client/src/components/Story/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import { StoryProvider } from './StoryContext';
 import { CharacterProvider } from './CharacterContext';
-import StoryPrompt from './StoryPrompt';
-import ChoiceBox from './ChoiceBox';
-import DebugConsole from './DebugConsole';
+import StoryPrompt from '../StoryPrompt';
+import ChoiceBox from '../ChoiceBox';
+import DebugConsole from '../DebugConsole';
 
 export default function Story() {
 
