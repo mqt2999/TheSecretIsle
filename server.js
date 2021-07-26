c442b73d7a3b3d2ffa4ccbc7911182bf94aa4855
Janei <18485105+janeijones@users.noreply.github.com>
2021-07-26 11:32:13 -0500

    Changed session to mongo

--- server.js
+++ server.js
@@ -5,42 +5,8 @@ const app = express();
 const PORT = process.env.PORT || 3001;
 const routes = require("./routes/index.js")
 const path = require('path')
-
 const session = require('express-session');
-const MongoDBStore = require('connect-mongodb-session')(session);
-
-const store = new MongoDBStore({
-    url: 'mongodb://localhost/secretIsleGame',
-    // databaseName:'secretIsleGame',
-    collection: 'mySessions'
-  });
-
-  store.on('error', function(error) {
-      console.log(error);
-    });
-    
-
-    app.use(session({
-      secret: 'This is a secret',
-      cookie: {},
-      store: store,
-      resave: false,
-      saveUninitialized: true
-    }));
-
-    app.get('/', function(req, res) {
-      res.send('Hello ' + JSON.stringify(req.session));
-    });
-// const routes = require('./routes')
-
-// middleware
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
-
-app.use(express.static(path.join(__dirname, 'public'))); 
-
-// Add routes, both API and view
-app.use(routes);
+const MongoStore = require('connect-mongo');
 
 //Connect to the Mongo DB
 mongoose.connect(
@@ -53,9 +19,40 @@ mongoose.connect(
   } 
 );
 
+const store = new MongoStore({
+    url: 'mongodb://localhost/secretIsleGame',
+    // databaseName:'secretIsleGame',
+    collection: 'mySessions'
+  });
+
+  store.on('error', function(error) {
+      console.log(error);
+    });
+    
+
+    app.use(session({
+      secret: 'secret',
+      cookie: {},
+      store: store,
+      resave: false,
+      saveUninitialized: true
+    }));
+
+  
+// const routes = require('./routes')
+
+// middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, 'public'))); 
+
+// Add routes, both API and view
+app.use(routes);
+
+
+
 // Start the API server
 app.listen(PORT, function() {
   console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-
-  // throw new Error ('BROKEN')
 })
