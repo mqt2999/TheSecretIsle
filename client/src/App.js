ca61706752d7c06df52660fd744931c81ae5f123
Nate Worley <immythz@gmail.com>
2021-07-27 03:51:50 -0500

    Added Switch

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './App.css';
 import Story from './pages/Story';
 import Loading from './pages/Loading'
@@ -11,19 +11,22 @@ import Main from './components/Main/Main'
 import EndGame from './components/EndGame/EndGame';
 import Minigame from './pages/Minigame'
 
+
 function App() {
   return (
     <Router>
-      <Main>
-          <Route exact path="/" component={Home} />
-          <Route exact path="/Story" component={Story} />
-          <Route exact path="/Loading" component={Loading} />
-          <Route exact path="/Login" component={Login} />
-          <Route exact path="/SignUp" component={SignUp} />
-          <Route exact path="/Quiz" component={Quiz} />
-          <Route exact path="/Minigame" component={Minigame} />
-          <Route exact path="/EndGame" component={EndGame} />
-      </Main>
+        <Main>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/Story" component={Story} />
+            <Route exact path="/Loading" component={Loading} />
+            <Route exact path="/Login" component={Login} />
+            <Route exact path="/SignUp" component={SignUp} />
+            <Route exact path="/Quiz" component={Quiz} />
+            <Route exact path="/Minigame" component={Minigame} />
+            <Route exact path="/EndGame" component={EndGame} />
+          </Switch>
+        </Main>
     </Router>
   );
 }
