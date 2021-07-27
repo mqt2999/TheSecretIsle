770b51d89cee62d341615a1871f349ac595e14f9
Nate Worley <immythz@gmail.com>
2021-07-27 01:03:55 -0500

    fixed Trivia Quiz page and API call

--- client/src/components/Trivia/Trivia.js
+++ client/src/components/Trivia/Trivia.js
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
 import { useStory } from '../Story/StoryContext';
+import { useTrivia } from '../QuizContext/QuizContext'
 import './style.css';
 
-export default function Trivia(props) {
-    
-   console.log("questions",props.questions)
+export default function Trivia() {
+    const triviaArray =  useTrivia()
     return (
         <div className="minigame mt-3 mb-5">
-            <h3> {props.a[0].question} </h3>
+            <h3> {triviaArray[0].question} </h3>
         </div>
     )
 }
--- client/src/pages/Quiz.js
+++ client/src/pages/Quiz.js
@@ -7,6 +7,7 @@ import Trivia from "../components/Trivia/Trivia";
 import ChoiceBoxQuiz from "../components/ChoiceBoxQuiz/ChoiceBoxQuiz";
 import { StoryProvider } from "../components/Story/StoryContext";
 import { CharacterProvider } from "../components/Story/CharacterContext";
+import { TriviaProvider } from "../components/QuizContext/QuizContext"
 
 
 
@@ -14,37 +15,30 @@ function Quiz() {
     const [questions, setQuestions]= useState({})
     const [riddle, setRiddle] = useState({})
     const [miniGame, setMiniGame] = useState({}) 
-    const [trivia, setTrivia] = useState({})
+  
 
-    useEffect( () => {
-        getQuizQuestions()
-        getTrivia()
-    },[])
+    // useEffect( () => {
+    //     getQuizQuestions()
+    // },[])
 
-    function getQuizQuestions() {
-        axios.get("/api/story/")
-            .then(res => {
+    // function getQuizQuestions() {
+    //     axios.get("/api/story/")
+    //         .then(res => {
                 
-                setQuestions(res.data)
-              })
+    //             setQuestions(res.data)
+    //           })
         
-    }
-    function getTrivia(){
-        axios.get("https://opentdb.com/api.php?amount=10&category=22&difficulty=medium&type=boolean")
-        .then(res => {
-          setTrivia(res.data.results)
-          console.log("hehehuey", res.data.results)
-        })
-    }
-
+    // }
       return(
         <ContainerBig>
             <Navbar />
             <StoryProvider>
                 <CharacterProvider>
                     <StoryPrompt />
-                    <Trivia a={trivia}/>
+                    <TriviaProvider>
+                    <Trivia/>
                     <ChoiceBoxQuiz />
+                    </TriviaProvider>
                 </CharacterProvider>
             </StoryProvider>
         </ContainerBig>
